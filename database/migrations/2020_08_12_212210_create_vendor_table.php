9d6cf4074791d3b9e82c550f05fccfc910561fe7
Róbert Dékány <dekany.r123@gmail.com>
2020-08-12 23:36:33 +0200

    create Vendor model and migration

---
+++ database/migrations/2020_08_12_212210_create_vendor_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVendorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendor', function (Blueprint $table) {
+            $table->foreignId('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendor');
+    }
+}
